697e9eea3c24fd979ab16a76b6828a18a68f29c4
Mandava-Kamal-09 <99558200+Mandava-Kamal-09@users.noreply.github.com>
2026-01-27 08:28:39 +0000

    Deploying to gh-pages from @ Mandava-Kamal-09/Mandava-Kamal-09.github.io@d1447b50734bc34bdf01783ecff34f311c49af24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-tools",
           title: "Tools",
-          description: "Interactive statistical analysis tools and dashboards.",
+          description: "Interactive tools and dashboards for statistical analysis and dynamical systems.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/tools/";
